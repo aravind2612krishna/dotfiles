b2e06624b14731b06f2f3fca0de5a180e899ee2d
aravk <aravk@altair.com>
2024-04-02 11:46:17 +0530

    Minor changes to editorconfig

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # mkdir -p ~/.config/zellij/
 rsync --archive --delete zellij/*.kdl ~/.config/zellij
-rsync --archive zellij/\.* ~ 
-
+rsync --archive zellij/\.* ~
+([[ ! -d ~/.vim/pack/osc/start/ ]] && mkdir -p ~/.vim/pack/osc/start/ && cd ~/.vim/pack/osc/start/ && git clone https://github.com/ojroques/vim-oscyank)
+rsync --archive .editorconfig ~/.editorconfig
