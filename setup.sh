4df87a909aab30c7199a3cd04b34ff4014140f43
aravk <aravk@altair.com>
2024-04-15 14:11:45 +0530

    Changes for auto-setup of osc-yank

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
+
 # mkdir -p ~/.config/zellij/
 rsync --archive --delete zellij/*.kdl ~/.config/zellij
 rsync --archive zellij/\.* ~
-([[ ! -d ~/.vim/pack/osc/start/ ]] && mkdir -p ~/.vim/pack/osc/start/ && cd ~/.vim/pack/osc/start/ && git clone https://github.com/ojroques/vim-oscyank)
 rsync --archive .editorconfig ~/.editorconfig
+
+mkdir -p ~/.vim/pack/osc/start/
+(cd ~/.vim/pack/osc/start && [ ! -d "vim-oscyank" ] && git clone https://github.com/ojroques/vim-oscyank)
+(cd ~/.vim/pack/osc/start && [ ! -d "vim-oscyank" ] && git clone git@github.com:ojroques/vim-oscyank.git)
