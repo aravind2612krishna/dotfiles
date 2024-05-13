92b0895ccd1f566a4acd737419873953868e815c
aravk <aravk@altair.com>
2024-05-12 21:52:12 -0400

    VimOSC

--- setup.sh
+++ setup.sh
@@ -3,8 +3,12 @@
 # mkdir -p ~/.config/zellij/
 rsync --archive --delete zellij/*.kdl ~/.config/zellij
 rsync --archive zellij/\.* ~
-rsync --archive .editorconfig ~/.editorconfig
+rsync --archive editorconfig ~/.editorconfig
 
 mkdir -p ~/.vim/pack/osc/start/
 (cd ~/.vim/pack/osc/start && [ ! -d "vim-oscyank" ] && git clone https://github.com/ojroques/vim-oscyank)
 (cd ~/.vim/pack/osc/start && [ ! -d "vim-oscyank" ] && git clone git@github.com:ojroques/vim-oscyank.git)
+
+echo "set runtimepath+=$(cd vim/; pwd)" > ~/.vimrc
+echo "set runtimepath+=$(cd ~/.vim/pack/osc/start/vim-oscyank; pwd)" >> ~/.vimrc
+echo "runtime vimrc" >> ~/.vimrc
